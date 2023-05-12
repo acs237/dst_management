775355975cf34fa90d32c18d406b390e474a4708
acs237 <acs23072000@gmail.com>
2023-05-13 01:10:08 +1000

    add dataInput.ts file containing function templates

---
+++ dataInput.ts
@@ -0,0 +1,29 @@
+import { item, quantity } from './types';
+
+export function addItem(code: string, name: string, categoryId: number, unitId: number, price: number) {
+    
+}
+
+export function addCategory(name: string) {
+
+}
+
+export function addUnit(name: string, numberItems: number) {
+
+}
+
+export function importItems(itemsList: Array<{item: item, quantity: quantity}>) {
+
+}
+
+export function exportItems(itemsList: Array<{item: item, quantity: quantity}>) {
+
+}
+
+export function singleItemLog(code: string) {
+
+}
+
+export function multipleItemsLog() {
+    
+}
