7b53f9ccd38a96e474b2ad594bc02116d69a6181
acs237 <acs23072000@gmail.com>
2023-05-14 20:48:43 +1000

    add implementation for getData and setData

--- data.ts
+++ data.ts
@@ -1,11 +1,12 @@
-import { item } from './types';
-
-const data = {items: [] as item[]};
+import { readFileSync, writeFileSync, existsSync } from 'fs';
+let data = {};
 
 export function getData() {
-
+    if (existsSync('./data.json')) {
+        data = JSON.parse(readFileSync('./data.json').toString()) ;
+    }
 }
 
 export function setData() {
-    
+    writeFileSync('./data.json', JSON.stringify(data));
 }
