5e25d4da3edfced1d56e812f2656e15dcc7ba369
acs237 <acs23072000@gmail.com>
2023-05-17 18:42:13 +1000

    add clear function in data.ts

--- data.ts
+++ data.ts
@@ -1,12 +1,19 @@
 import { readFileSync, writeFileSync, existsSync } from 'fs';
-let data = {};
+import { item, data, category, unit, log } from './types';
+let data = {items: [] as item[], categories: [] as category[], units: [] as unit[], logs: [] as log[]};
 
 export function getData() {
     if (existsSync('./data.json')) {
-        data = JSON.parse(readFileSync('./data.json').toString()) ;
+        data = JSON.parse(readFileSync('./data.json').toString());
     }
+    return data;
 }
 
-export function setData() {
+export function setData(data: data) {
+    writeFileSync('./data.json', JSON.stringify(data));
+}
+
+export function clearData() {
+    data = {items: [] as item[], categories: [] as category[], units: [] as unit[], logs: [] as log[]};
     writeFileSync('./data.json', JSON.stringify(data));
 }
